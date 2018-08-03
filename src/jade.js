ca84abe55cfeb334d383a17b10dcf2dc20c8b660
DanielJelev <jinjaaar@gamil.com>
2018-08-03 16:02:36 +0300

    Implement jfor and jif html helpers

--- src/jade.js
+++ src/jade.js
@@ -26,6 +26,23 @@ var Route = (function () {
         this.component = component;
     }
 
+    Route.prototype.checkForParam = function () {
+        if (this.path.indexOf(":") > -1) {
+            this.setRouteParams();
+            return true;
+        }
+    }
+
+    Route.prototype.setRouteParams = function () {
+
+        console.log();
+
+
+    }
+
+    Route.prototype.getBasePath = function () {
+        return this.path
+    }
     return Route;
 
 }());
@@ -34,8 +51,17 @@ var Route = (function () {
 var ViewEngine = (function () {
 
     function ViewEngine(scope) {
-        this.inputElements = document.querySelectorAll('[jmodel]');
-        this.boundElements = document.querySelectorAll('[jbind]');
+        this.modelSelector = '[jmodel]'
+        this.bindSelector = '[jbind]';
+        this.repeaterSelector = '[jfor]';
+        this.viewModelSelector = "[jvm]"
+        this.ifStatmentsSelector = "[jif]"
+
+        this.inputElements = document.querySelectorAll(this.modelSelector);
+        this.boundElements = document.querySelectorAll(this.bindSelector);
+        this.repeatElements = document.querySelectorAll(this.repeaterSelector);
+        this.viewModelElements = document.querySelectorAll(this.viewModelSelector)
+        this.ifStatemetnsElements = document.querySelectorAll(this.ifStatmentsSelector)
         this.scope = scope;
     }
 
@@ -56,6 +82,22 @@ var ViewEngine = (function () {
             }
         }
 
+        for (var el of this.repeatElements) {
+            var propName = el.getAttribute('jfor');
+            this.setPropUpdateLogic(propName);
+        }
+
+        for (var el of this.viewModelElements) {
+            var propName = el.getAttribute('jvm');
+            this.setPropUpdateLogic(propName);
+        }
+
+        for (var el of this.ifStatemetnsElements) {
+            var propName = el.getAttribute('jif');
+            this.setPropUpdateLogic(propName);
+            
+        }
+
         return this.scope;
     }
 
@@ -66,31 +108,132 @@ var ViewEngine = (function () {
 
             Object.defineProperty(this.scope, prop, {
                 // Automatically update bound dom elements when a scope property is set to a new value
-                set: (newValue) => {
+                set: function (newValue) {
                     value = newValue;
+                    that.setModeValue(newValue, prop);
+                    that.setBindValue(newValue, prop);
+                    that.setRepeaterValue(newValue, prop);
+                    that.setViewModelValue(newValue, prop);
+                    that.setIfStatementValue(prop)
 
-                    // Set input elements to new value
-                    for (var el of that.inputElements) {
-                        if (el.getAttribute('jmodel') === prop) {
-                            if (el.type) {
-                                el.value = newValue;
-                            }
-                        }
-                    }
-                    // Set all other bound dom elements to new value
-                    for (var el of that.boundElements) {
-                        if (el.getAttribute('jbind') === prop) {
-                            if (!el.type) {
-                                el.innerHTML = newValue;
-                            }
-                        }
-                    }
                 },
-                get: () => {
+                get: function () {
                     return value;
                 },
                 enumerable: true
             })
+        }
+    }
+
+    ViewEngine.prototype.setModeValue = function (newValue, prop) {
+        // Set input elements to new value
+        for (var el of this.inputElements) {
+            if (el.getAttribute('jmodel') === prop) {
+                if (el.type) {
+                    el.value = newValue;
+                }
+            }
+        }
+    }
+
+    ViewEngine.prototype.setBindValue = function (newValue, prop) {
+        // Set all other bound dom elements to new value
+        for (var el of this.boundElements) {
+            if (el.getAttribute('jbind') === prop) {
+                if (!el.type) {
+                    el.innerHTML = newValue;
+                }
+            }
+        }
+    }
+
+    ViewEngine.prototype.setRepeaterValue = function (newValue, prop) {
+        // Set all object values and repeat html
+        for (var el of this.repeatElements) {
+            if (el.getAttribute('jfor') === prop) {
+                if (Array.isArray(newValue)) {
+                    var result = ""
+                    for (var i in newValue) {
+                        var a = newValue[i];
+                        var templateRow = el.innerHTML;
+
+                        for (var j in a) {
+                            var current = a[j];
+                            var regex = '{' + prop + '.' + j + '}'
+                            templateRow = templateRow.replace(regex, current)
+                        }
+                        result += templateRow
+                    }
+                    el.innerHTML = result;
+                }
+            }
+        }
+    }
+
+    ViewEngine.prototype.setViewModelValue = function (newValue, prop) {
+        // Set object values or normal value and repeat html
+        for (var el of this.viewModelElements) {
+            if (el.getAttribute('jvm') === prop) {
+                debugger
+                if (typeof newValue === 'object') {
+                    var templateRow = el.innerHTML;
+                    for (var i in newValue) {
+                        var current = newValue[i];
+                        var regex = '{' + prop + '.' + i + '}'
+                        templateRow = templateRow.replace(regex, current)
+                    }
+                    el.innerHTML = templateRow;
+                } else {
+                    var templateRow = el.innerHTML;
+                    var regex = '{' + prop + '}'
+                    templateRow = templateRow.replace(regex, newValue)
+                    el.innerHTML = templateRow;
+                }
+            }
+        }
+    }
+
+    ViewEngine.prototype.setIfStatementValue = function (prop) {
+        var that = this;
+        for (var el of this.ifStatemetnsElements) {
+            if (el.getAttribute('jif').indexOf(prop) > -1) {
+                if (prop) {
+                    var attribute = el.getAttribute('jif');
+                    var props = getScopePropertyFromExpression(attribute);
+                    var expression = replaceExpression(props, attribute);
+                    console.log(that.scope)
+                    var evalCondition = eval(expression);
+
+                    if (evalCondition === true || typeof evalCondition === 'object') {
+                        el.style.display = 'block';
+                    }
+                    else {
+                        el.style.display = 'none';
+                    }
+                }
+            }
+        }
+
+        function getScopePropertyFromExpression(expression){
+            var properties = [];
+            for(var i in that.scope){
+                if(expression.indexOf(i) > -1 && expression != i){
+                   properties.push(i)
+                }
+            }
+
+            return properties;
+        }
+
+        function replaceExpression(properties, expression){
+
+            for (var i in properties){
+                debugger
+                var currentProp = properties[i];
+                expression = expression.replace(currentProp,"that.scope."+currentProp)
+            }
+
+            return expression;
         }
     }
 
@@ -120,10 +263,6 @@ var Component = (function () {
         } else {
             element.innerHTML = this.template;
         }
-
-    }
-
-    Component.prototype.setTemplateUrl = function () {
 
     }
 
@@ -236,6 +375,10 @@ var Jade = (function () {
 
     Jade.prototype.getCurrentRoute = function () {
         var route = this.router.routes.filter(function (r) {
+            var isRouteHaveParam = r.checkForParam();
+            if (isRouteHaveParam) {
+                r.path = r.getBasePath();
+            }
             return location.href == r.path
         })[0];
 
